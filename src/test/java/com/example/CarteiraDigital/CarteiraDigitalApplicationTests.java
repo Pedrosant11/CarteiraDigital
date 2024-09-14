7116dcab594c2898f2db481b27419452238eb952
Pedro Henrique <pedrohenrique1108@gmail.com>
2024-09-14 19:43:26 -0300

    Criado projeto do Spring

---
+++ src/test/java/com/example/CarteiraDigital/CarteiraDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarteiraDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
